2ef3c2e1a1b75d49b6ccdda308db16221a3bf4d6
ArlinskiEV <ArlinskiEV@gmail.com>
2018-03-02 06:17:02 +0300

    longest-consecutive-sequence/fix: REALLY???...

--- src/index.js
+++ src/index.js
@@ -5,26 +5,28 @@ module.exports = function longestConsecutiveLength(array) {
   let result = 0;
   
   for (let i = 0; i < array.length; i++) {
-    temp[array[i]] = {length: {current: 1, idCurrent: array[i]}, idLength: array[i]};
+    if (!temp[array[i]]) {
+       temp[array[i]] = {length: {current: 1, idCurrent: array[i]}, idLength: array[i]};
 
-    //check left
-    if (temp[array[i] - 1]) {
-      temp[array[i] - 1].length.current += temp[array[i]].length.current;
-      temp[array[i]] = temp[array[i] - 1];
-    }
-
-    //check right
-    if (temp[array[i] + 1]) {
-      temp[array[i] + 1].length.current += temp[array[i]].length.current;
-      temp[array[i]] = temp[array[i] + 1];
-
-      //if in middle
+      //check left
       if (temp[array[i] - 1]) {
-        temp[array[i] - 1].length = temp[array[i]].length;
-      };
-    }
+        temp[array[i] - 1].length.current += temp[array[i]].length.current;
+        temp[array[i]] = temp[array[i] - 1];
+      }
 
-    if (result < temp[array[i]].length.current) result = temp[array[i]].length.current;
+      //check right
+      if (temp[array[i] + 1]) {
+        temp[array[i] + 1].length.current += temp[array[i]].length.current;
+        temp[array[i]] = temp[array[i] + 1];
+
+        //if in middle
+        if (temp[array[i] - 1]) {
+          temp[array[i] - 1].length = temp[array[i]].length;
+        };
+      }
+
+      if (result < temp[array[i]].length.current) result = temp[array[i]].length.current;
+    }
   }
 
   debugger;
